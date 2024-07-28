b8bbf305108c69b33cab76edda2d24b6239f8ccd
Abhishek Sharma <akstenx@gmail.com>
2024-07-28 15:14:57 +0530

    latest changes

---
+++ apps/web/src/app/api/hello/route.ts
@@ -0,0 +1,14 @@
+import { db } from "@/lib/db"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  const users = await db.user.findMany()
+
+  return NextResponse.json(
+    {
+      message: "OK",
+      data: users,
+    },
+    { status: 200 }
+  )
+}
