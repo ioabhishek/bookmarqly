b3a7abe872a36dc35c168f9dc7cb5ea403099533
Abhishek Sharma <akstenx@gmail.com>
2024-08-18 14:57:28 +0530

    CORS ERROR FIX

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -24,4 +24,25 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ]
+  },
 }
