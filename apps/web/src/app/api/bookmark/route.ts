da3ebfb0c94b4f67b4405e23a950e975c889677d
Abhishek Sharma <akstenx@gmail.com>
2024-08-20 16:12:23 +0530

    added user agents for fetching og data

--- apps/web/src/app/api/bookmark/route.ts
+++ apps/web/src/app/api/bookmark/route.ts
@@ -154,7 +154,12 @@ export async function POST(req: NextRequest) {
 
   const { url, note, collectionId } = validationResult.data
 
-  const { data: html } = await axios.get(url)
+  const { data: html } = await axios.get(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    },
+  })
 
   // Load the HTML into cheerio
   const $ = cheerio.load(html)
